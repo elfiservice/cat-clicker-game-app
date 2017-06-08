64bb1e8ae77fe875f64137189a6b29f0d036bddb
elfiservice <armandojunior@Armandos-MacBook-Pro.local>
2017-06-08 08:21:13 -0300

    feat: add 1 more cat and show yours names and clicks for each

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
-var counterClickDiv = $(".count_click_cat");
-var counterClicks = 0;
+var nameCat1 = "Garfield";
+var nameCat2 = "Puff";
+var cat1Image = $(".cat1");
+var cat2Image = $(".cat2");
+var counterClickDivCat1 = $(".count_click_cat1");
+var counterClickDivCat2 = $(".count_click_cat2");
+var counterClicksCat1 = 0;
+var counterClicksCat2 = 0;
 var emojiSad = "😞";
 var emojiSmile = "😊";
 var emojiSuperSmile = "😁";
 
-if(counterClicks === 0) {
-  counterClickDiv.text("No cliks Yet " + emojiSad);
+//show the names of the Cats
+$("#cat1").prepend("<h3>" + nameCat1 + "</h3>");
+$("#cat2").prepend("<h3>" + nameCat2 + "</h3>");
+
+//Show this texts When have not cliks
+if(counterClicksCat1 === 0) {
+  counterClickDivCat1.text("No cliks Yet " + emojiSad);
+}
+if (counterClicksCat2 === 0 ) {
+  counterClickDivCat2.text("No cliks Yet " + emojiSad);
 }
 
-$(".cat_img").click(function(){
-  counterClickDiv.text("You clicked " + counterClicks++ + " times! " + (counterClicks > 10 ? emojiSuperSmile : emojiSmile));
+//Show numbers of cliks each cat had
+cat1Image.click(function(){
+  counterClickDivCat1.text("You clicked " + counterClicksCat1++ + " times! " + (counterClicksCat1 > 10 ? emojiSuperSmile : emojiSmile));
+});
+
+cat2Image.click(function(){
+  counterClickDivCat2.text("You clicked " + counterClicksCat2++ + " times! " + (counterClicksCat2 > 10 ? emojiSuperSmile : emojiSmile));
 });

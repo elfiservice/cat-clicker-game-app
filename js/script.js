0cbea287a7a2a2adfab37adad9283baffb20476b
elfiservice <armandojunior@Armandos-MacBook-Pro.local>
2017-06-09 08:21:02 -0300

    feat: add Cat to Objects to be scaleable - created list of cats - show image cat after clicked in your name - count clicks on cat's image

--- js/script.js
+++ js/script.js
@@ -1,32 +1,65 @@
-var nameCat1 = "Garfield";
-var nameCat2 = "Puff";
-var cat1Image = $(".cat1");
-var cat2Image = $(".cat2");
-var counterClickDivCat1 = $(".count_click_cat1");
-var counterClickDivCat2 = $(".count_click_cat2");
-var counterClicksCat1 = 0;
-var counterClicksCat2 = 0;
+var counterClickDivCat = $(".count_click_cat");
+
+
 var emojiSad = "😞";
 var emojiSmile = "😊";
 var emojiSuperSmile = "😁";
 
-//show the names of the Cats
-$("#cat1").prepend("<h3>" + nameCat1 + "</h3>");
-$("#cat2").prepend("<h3>" + nameCat2 + "</h3>");
+var Cat = function(name, image){
+  this.name = name;
+  this.counterClicksCat = 0;
+  this.image = image;
+};
 
-//Show this texts When have not cliks
-if(counterClicksCat1 === 0) {
-  counterClickDivCat1.text("No cliks Yet " + emojiSad);
-}
-if (counterClicksCat2 === 0 ) {
-  counterClickDivCat2.text("No cliks Yet " + emojiSad);
-}
+var listOfCats = [
+  new Cat("Chani", "cat1.jpg"),
+  new Cat("PUff", "cat2.jpg"),
+  new Cat("Garfield", "cat3.jpg"),
+  new Cat("Pill", "cat4.jpeg"),
+  new Cat("Cutee", "cat5.jpg")
+];
 
-//Show numbers of cliks each cat had
-cat1Image.click(function(){
-  counterClickDivCat1.text("You clicked " + counterClicksCat1++ + " times! " + (counterClicksCat1 > 10 ? emojiSuperSmile : emojiSmile));
+listOfCats.forEach(function(cat){
+    $(".cat-list").append("<li class='cat-" + cat.name + "'>" + cat.name + "</li>");
+
+     $("li.cat-" + cat.name).click((function(nameCopy) {
+        return function() {
+            // alert(nameCopy);
+            $("#cat h3").text(cat.name);
+            $(".img_content>img")
+              .attr("src","images/" + cat.image)
+              .attr("alt","Image of Cute Cat called " + cat.name)
+              .removeClass()
+              .addClass(cat.name);
+
+              if(cat.counterClicksCat === 0) {
+                counterClickDivCat.text("No cliks Yet " + emojiSad);
+              } else {
+                counterClickDivCat.text("You clicked " + cat.counterClicksCat + " times! " + (cat.counterClicksCat > 10 ? emojiSuperSmile : emojiSmile));
+              }
+
+        };
+    })(cat.name));
+
+
 });
 
-cat2Image.click(function(){
-  counterClickDivCat2.text("You clicked " + counterClicksCat2++ + " times! " + (counterClicksCat2 > 10 ? emojiSuperSmile : emojiSmile));
+
+
+$("#cat img").click(function(){
+  var classNameCat = $("#cat img").attr("class");
+  listOfCats.forEach(function(cat){
+    if (cat.name == classNameCat) {
+      cat.counterClicksCat++;
+
+      //FAzer uma FUNCAO para o Objeto CAT
+      if(cat.counterClicksCat === 0) {
+        counterClickDivCat.text("No cliks Yet " + emojiSad);
+      } else {
+        counterClickDivCat.text("You clicked " + cat.counterClicksCat + " times! " + (cat.counterClicksCat > 10 ? emojiSuperSmile : emojiSmile));
+      }
+      //
+      // console.log(cat.counterClicksCat);
+    }
+  });
 });
